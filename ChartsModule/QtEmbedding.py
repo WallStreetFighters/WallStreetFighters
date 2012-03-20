b56654fbdd7d2640f3bd84291c87e22bc0a7cf06
Andrzej Smoliński <as40@wp.pl>
2012-03-20 10:00:40 +0100

    Poprawiono zakres osi na wykresie logarytmicznym, zmniejszono etykiety i zwiększono marginesy

--- ChartsModule/QtEmbedding.py
+++ ChartsModule/QtEmbedding.py
@@ -18,8 +18,8 @@ class ApplicationWindow(QtGui.QMainWindow):
         l = QtGui.QVBoxLayout(self.main_widget)
         parser.loadData()         
 
-        finObj = parser.createWithCurrentValueFromYahoo(parser.STOCK_LIST[4][1],
-        parser.STOCK_LIST[4][0],'stock',parser.STOCK_LIST[4][3]) 
+        finObj = parser.createWithCurrentValueFromYahoo(parser.STOCK_LIST[6][1],
+        parser.STOCK_LIST[6][0],'stock',parser.STOCK_LIST[6][3]) 
         finObj.updateArchive() 
         chart = Chart(self.main_widget, finObj)                
         l.addWidget(chart)                        
@@ -28,8 +28,9 @@ class ApplicationWindow(QtGui.QMainWindow):
         chart.setOscPlot('RSI')
         chart.setDrawingMode(True)                
         chart.setMainIndicator('SMA')
-        chart.setData(finObj,datetime.datetime(2009,10,1),datetime.datetime(2010,11,1),'daily')
+        chart.setData(finObj,datetime.datetime(2011,10,1),datetime.datetime(2012,01,1),'daily')
         chart.setMainType('candlestick')                        
+        chart.setScaleType('log')          
         print "strorzyłem wykresa"
         
 qApp = QtGui.QApplication(sys.argv)
