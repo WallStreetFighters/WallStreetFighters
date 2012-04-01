3e06348ec48f9e73a54702f88403c8da376f4c51
pawelqus <pawelqus@gmail.com>
2012-04-01 19:37:18 +0200

    dodano formacje glowy i ramion

--- TechAnalysisModule/trendAnalysis.py
+++ TechAnalysisModule/trendAnalysis.py
@@ -24,12 +24,18 @@ def trend(a):
     if (angle <-trendVul and angle > -90):
             return -1 # malejacy
 
-def aInRect(array):
-    """Sprawdzamy czy punkty w tablicy naleza do prostej +/- rectVul"""
+def linearFun(array):
     if array.size < 2:
-        return 0
+        return 0, 0
     a = (array[1]-array[0])*1.0
     b = array[0]
+    return a,b
+
+def aInRect(array):
+    """Sprawdzamy czy punkty w tablicy naleza do prostej +/- rectVul"""
+    a, b = linearFun(array)
+    if a == 0:
+        return 0
     for i in range(2,array.size):
             y = a*i+b
             if y > (1+rectVul)*array[i] or y < (1-rectVul)*array[i]:
@@ -66,3 +72,56 @@ def findMaxMin(array):
             res = y[z.index(max(z))]
             break
     return sup, res    
+
+    
+def headAndShoulders(values, volumine):
+    if values.size != 3 or volumine.size != 3:
+        return
+    leftArmVal = values[0]
+    headVal = values[1]
+    rightArmVal = values[2]
+    leftArmVol = volumine[0]
+    headVol = volumine[1]
+    rightArmVol = volumine[2]
+    maxLeftArmVal = max(leftArmVal)
+    maxLeftArmVol = max(leftArmVol)
+    maxHeadVal = max(headVal)
+    maxHeadVol = max(headVol)
+    maxRightArmVol = max(rightArmVol)
+    maxRightArmVal = max(rightArmVal)
+
+    #Wartosc lewego ramienia < glowy i wartosc wolumenu lewego ramienia ma byc najwieksza
+    if maxLeftArmVal > maxHeadVal  or maxRightArmVal > maxHeadVal or maxLeftArmVol < maxHeadVol or maxLeftArmVol < maxRightArmVol:
+        return 0
+
+    #wartosc prawego ramienia nie moze zbyt odbiegac od wartosci lewego
+    if maxRightArmVal > 1.1 * maxLeftArmVal or maxRightArmVal < 0.9*maxLeftArmVal:
+        return 0
+
+    #wolumin na formacji ma byc malejacy
+    if (trend(regression(list(leftArmVol)+list(headVol)+list(rightArmVol))) > -1):
+        return 0
+    
+    #wykreslamy linie szyi
+    minLeftArmVal = min(leftArmVal[leftArmVal.index(maxLeftArmVal):]) #min z prawej strony max lewego ramienia
+    minHeadVal = min(headVal[headVal.index(maxHeadVal):])
+    return 1    
+
+def lookForHeadAndShoulders(values, volumine):
+    """Szukamy formacji glowy i ramion"""
+    val = asarray(list(combinations(divideArray(values, div), 3)))
+    vol = asarray(list(combinations(divideArray(volumine, div), 3)))
+    map(headAndShoulders)
+    
+def headAndShouldersReversed(array):
+    """Szukamy odwroconej formacji glowy i ramion"""
+    return
+    
+    
+values = [1, 2, 10, 1, 2, 20, 1, 2, 12]
+values = asarray(values)
+volumin = [1, 2, 10, 1, 1, 1, 1, 1, 1]
+volumin = asarray(volumin)
+print values
+print volumin
+print lookForHeadAndShoulders(values, volumin)
