c8c2641433dc45781249c2ec94394fa01b1016b6
pawelqus <pawelqus@gmail.com>
2012-04-29 13:09:44 +0200

    added strategy

---
+++ TechAnalysisModule/strategy.py
@@ -0,0 +1,29 @@
+from TechAnalysisModule.candles import *
+import TechAnalysisModule.trendAnalysis as trend
+import TechAnalysisModule.oscilators as osc
+
+trendVal = 100
+"""Formacje"""
+"""Odwrocenie trendu wzrostowego"""
+headAndShouldersVal = 100
+risingWedgeVal = 80
+fallingTriangleVal = 80
+"""Odwrocenie trendu spadkowego"""
+reversedHeadAndShouldersVal = 100
+fallingWedgeVal = 80
+risingTriangleVal = 80
+"""Kontynuacja trendu"""
+symetricTriangleVal = 50
+rectangleVal = 30
+"""Wskazniki i oscylatory"""
+oscilatorsVal = 50
+# newHighNewLowVal - New High New Low Index
+# bollignerVal - Bollinger Bands
+# momentumVal - Momentum Oscillator
+# rocVal - ROC
+# cciVal - CCI
+# rsiVal - RSI
+# williamsVal - Williams Oscillator
+"""Luki"""
+
+"""Formacje swiecowe"""
