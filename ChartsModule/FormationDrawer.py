1a6ff0353c575d06b9304a76347346f5cb37f275
Andrzej Smoliński <as40@wp.pl>
2012-05-08 11:10:22 +0200

    kod zgodny ze sztuką

--- ChartsModule/FormationDrawer.py
+++ ChartsModule/FormationDrawer.py
@@ -6,30 +6,74 @@ class FormationDrawer:
     """Klasa odpowiedzialna za rysowanie wybranych przez użytkownika formacji
     (przekazywanych w tablicy configuration) na wykresie."""
     
-    def __init__(self, chart):
+    def __init__(self, chart, strategy=None):
         """chart = obiekt klasy Chart. Domyślnie ustawiamy pustą listę formacji."""
         self.chart=chart
-        self.configuration=[]
+        self.strategy=strategy
     
-    def setConfiguration(config):
-        """Ustawiamy listę formacji, które będziemy rysować. config jest tablicą krotek postaci
-        (nazwa,kolor,linestyle,linewidth) Rozpoznawane nazwy są takie jak te w wartościach
-        zwracanych przez funkcje znajdujące formacje, oraz 'trend' """
-        self.configuration=config
+    def setFormations(self, s):
+        """Ustawiamy listę formacji, które będziemy rysować, poprzez przekazanie
+        obiektu klasy Strategy. Narysowane zostaną formacje o niezerowej wartości"""        
+        self.configuration=[]
+        if abs(s.trendVal)>0:
+            self.configuration.append(('trend',self.trendColor,self.trendLwidth,self.trendLstyle))
+        if abs(s.headAndShouldersVal)>0:
+            self.configuration.append(('head_shoulders',self.headShouldersColor,self.headShouldersLwidth,self.headShouldersLstyle))
+        if abs(s.tripleTopVal)>0:
+            self.configuration.append(('triple_top',self.tripleTopColor,self.tripleTopLwidth,self.tripleTopLstyle))        
+        if abs(s.risingWedgeVal)>0:
+            self.configuration.append(('rising_wedge',self.risingWedgeColor,self.risingWedgeLwidth,self.risingWedgeLstyle))        
+        if abs(s.fallingTriangleVal)>0:
+            self.configuration.append(('falling_triangle',self.fallingTriangleColor,self.fallingTriangleLwidth,self.fallingTriangleLstyle))        
+        if abs(s.reversedHeadAndShouldersVal)>0:
+            self.configuration.append(('reversed_head_shoulders',self.reversedHeadShouldersColor,self.reversedHeadShouldersLwidth,self.reversedHeadShouldersLstyle))        
+        if abs(s.tripleBottomVal)>0:
+            self.configuration.append(('triple_bottom',self.tripleBottomColor,self.tripleBottomLwidth,self.tripleBottomLstyle))        
+        if abs(s.fallingWedgeVal)>0:
+            self.configuration.append(('falling_wedge',self.fallingWedgeColor,self.fallingWedgeLwidth,self.fallingWedgeLstyle))                        
+        if abs(s.risingTriangleVal)>0:
+            self.configuration.append(('rising_triangle',self.risingTriangleColor,self.risingTriangleLwidth,self.risingTriangleLstyle))                                                                                          
+        if abs(s.symetricTriangleVal)>0:
+            self.configuration.append(('symmetric_triangle',self.symetricTriangleColor,self.symetricTriangleLwidth,self.symetricTriangleLstyle))                                                                                          
+        if abs(s.risingBreakawayGapVal)>0:
+            self.configuration.append(('rising_breakaway_gap',self.risingBreakawayGapColor,self.risingBreakawayGapLwidth,self.risingBreakawayGapLstyle))                                                                                          
+        if abs(s.risingContinuationGapVal)>0:
+            self.configuration.append(('rising_continuation_gap',self.risingContinuationGapColor,self.risingContinuationGapLwidth,self.risingContinuationGapLstyle))                                                                                          
+        if abs(s.risingExhaustionGapVal)>0:
+            self.configuration.append(('rising_exhaustion_gap',self.risingExhaustionGapColor,self.risingExhaustionGapLwidth,self.risingExhaustionGapLstyle))                                                                                          
+        if abs(s.fallingBreakawayGapVal)>0:
+            self.configuration.append(('falling_breakaway_gap',self.fallingBreakawayGapColor,self.fallingBreakawayGapLwidth,self.fallingBreakawayGapLstyle))                                                                                                  
+        if abs(s.fallingContinuationGapVal)>0:
+            self.configuration.append(('falling_breakaway_gap',self.fallingContinuationGapColor,self.fallingContinuationGapLwidth,self.fallingContinuationGapLstyle))                                                                                          
+        if abs(s.fallingExhaustionGapVal)>0:
+            self.configuration.append(('falling_continuation_gap',self.fallingExhaustionGapColor,self.fallingExhaustionGapLwidth,self.fallingExhaustionGapLstyle))                                                                                          
+        if abs(s.bull3Val)>0:
+            self.configuration.append(('bull3',self.bull3Color,self.bull3Lwidth,self.bull3Lstyle))                                                                                                      
+        if abs(s.bear3Val)>0:
+            self.configuration.append(('bear3',self.bear3Color,self.bear3Lwidth,self.bear3Lstyle))                                                                                          
+        if abs(s.mornigStarVal)>0:
+            self.configuration.append(('morning_star',self.mornigStarColor,self.mornigStarLwidth,self.mornigStarLstyle))                                                                                          
+        if abs(s.eveningStarVal)>0:
+            self.configuration.append(('evening_star',self.eveningStarColor,self.eveningStarLwidth,self.eveningStarLstyle))                                                                                                  
+        if abs(s.darkCloudVal)>0:
+            self.configuration.append(('dark_cloud',self.darkCloudColor,self.darkCloudLwidth,self.darkCloudLstyle))                                                                                          
+        if abs(s.piercingVal)>0:
+            self.configuration.append(('piercing',self.piercingColor,self.piercingLwidth,self.piercingLstyle)) 
+        #flag & penant nie mają w strategy wartości!
+                
 
     def drawFormations(self):
-        """Rysujemy formacje, które są zdefiniowane w tablicy configuration."""
+        """Rysujemy formacje, które są zdefiniowane w strategy."""
+        if self.strategy==None:
+            return
+        data=self.chart.getData()        
         geoForm=['rect', 'symmetric_triangle', 'rising_triangle', 'falling_triangle',
                 'rising_wedge', 'falling_wedge'] 
-        candleForm=['bull3','bear3','morning_star','evening_star','piercing','dark_cloud']
+        foundGeo=trend.findGeometricFormations(data.close)
+        candleForm=['bull3','bear3','morning_star','evening_star','piercing','dark_cloud']        
         gaps=['rising_breakaway_gap','rising_continuation_gap','rising_exhaustion_gap',
-              'falling_breakaway_gap','falling_continuation_gap','falling_exhaustion_gap']
-        fandp=['risingTrendFlagOrPennant','fallingTrendFlagOrPennant']                      
-        computedGeo=False
-        computedCandle=False
-        computedGaps=False
-        computedFandp=False
-        data=self.chart.getData()
+              'falling_breakaway_gap','falling_continuation_gap','falling_exhaustion_gap']        
+        fandp=['risingTrendFlagOrPennant','fallingTrendFlagOrPennant']                                
         self.chart.clearLines()
         self.chart.clearRectangles()
         for entry in self.configuration:
@@ -40,14 +84,14 @@ class FormationDrawer:
                     computedGeo=True
                 for formation in computedGeo:
                     if name==formation[0]:
-                        self.drawGeometricFormation(formation)
+                        self.drawGeometricFormation(formation,entry[1],entry[2],entry[3])
             elif name in candleForm:
                 if not computedCandle:
                     foundCandle=candles.findCandleFormations(data.open, data.high, data.low, data.close)
                     computedCandle=True
                 for formation in computedCandle:
                     if name==formation[0]:
-                        self.drawCandleFormation(formation)
+                        self.drawCandleFormation(formation,entry[1],entry[2],entry[3])
             elif name in gaps:
                 if not computedGaps:
                     foundGaps=candles.findGaps(data.high, data.low, data.close)
@@ -55,40 +99,28 @@ class FormationDrawer:
                 for gapsList in foundGaps:
                     for gap in gapsList:
                         if name==gap[0]:
-                            self.drawGap(gap)
+                            self.drawGap(gap,entry[1],entry[2],entry[3])
             elif name in fandp:
                 if not computedFandp:
                     #foundFandp=trend.findFlagsAndPennants(data.close, data.volume)
                     #tak to się wywołuje?
                     computedFandp=True
                 if name == foundFandp[0]:
-                    self.drawFlagAndPennant(foundFanp)                
+                    self.drawFlagAndPennant(foundFanp,entry[1],entry[2],entry[3])                
             elif name=='trend':
-                self.drawTrend()
+                self.drawTrend(entry[1],entry[2],entry[3])
             elif name=='rate_lines':
-                self.drawRateLines()        
+                self.drawRateLines(entry[1],entry[2],entry[3])        
             elif name=='head_shoulders':
-                self.drawHeadAndShoulders()
+                self.drawHeadAndShoulders(entry[1],entry[2],entry[3])
     
-    def drawGeometricFormation(self,formation):
-        for entry in settings:
-            if entry[0]==formation[0]:
-                color=entry[1]
-                lstyle=entry[2]
-                lwidth=entry[2]
-                break
+    def drawGeometricFormation(self,formation,color,lstyle,lwidth):        
         self.chart.drawLine(form[1][0], form[1][1], form[1][2], form[1][3], 
                             color, lwidth, lstyle)
         self.chart.drawLine(form[2][0], form[2][1], form[2][2], form[2][3], 
                             color, lwidth, lstyle)
 
-    def drawCandleFormation(self,formation):                        
-        for entry in settings:
-            if entry[0]==formation[0]:
-                color=entry[1]
-                lstyle=entry[2]
-                lwidth=entry[2]
-                break
+    def drawCandleFormation(self,formation,color,lstyle,lwidth):                                
         x=formation[1]-0.5
         y=0.97*min(self.data.low[formation[1]],self.data.low[formation[2]])
         width=formation[2]-formation[1]+1
@@ -97,13 +129,7 @@ class FormationDrawer:
         self.chart.drawRectangle(x,y,width,height,color,lwidth,lstyle)     
         
     
-    def drawGap(self,gap):
-        for entry in settings:
-            if entry[0]==gap[0]:
-                color=entry[1]
-                lstyle=entry[2]
-                lwidth=entry[2]
-                break
+    def drawGap(self,gap,color,lstyle,lwidth):        
         x=gap[1]
         width=1
         data=self.chart.getData()
@@ -113,21 +139,21 @@ class FormationDrawer:
         else:
             y=data.high[gap[1]+1]            
             height=data.low[gap[1]]-data.high[gap[1]+1]
-        self.chart.drawRectangle(x,y,width,height)
+        self.chart.drawRectangle(x,y,width,height,color,lwidth,lstyle)
     
-    def drawHeadAndShoulders(self,formation):
+    def drawHeadAndShoulders(self,formation,color,lstyle,lwidth):
         #uzupełnić
         pass
     
-    def drawFlagAndPennant(self,formation):
+    def drawFlagAndPennant(self,formation,color,lstyle,lwidth):
         #uzupełnić
         pass
 
-    def drawTrend(self):
+    def drawTrend(self,color,lstyle,lwidth):
         #uzupełnić
         pass
     
-    def drawRateLines(self):        
+    def drawRateLines(self,color,lstyle,lwidth):        
         values = trend.rateLines(array(self.chart.getData().close),0.38,0.62)
         print values
         self.drawLine(values[0][0],values[0][1],values[0][2],values[0][3],'y')
